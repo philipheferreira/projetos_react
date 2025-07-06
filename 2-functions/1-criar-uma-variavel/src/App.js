e8da266e825366331c586fe92cfde31cee0546a3
philipheferreira <philipheferreira@gmail.com>
2025-07-06 10:10:29 -0300

    Feat: finish simple project

---
+++ 2-functions/1-criar-uma-variavel/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  const nome = 'Philiphe Ferreira'
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Variavel local criada: <strong>{nome}</strong>.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
