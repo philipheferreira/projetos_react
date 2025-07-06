d1067df766fc24085c198cc2e8acb985cc4091bd
philipheferreira <philipheferreira@gmail.com>
2025-07-06 14:57:35 -0300

    Feat: finish project 4

--- 3-projects/4-botao-alerta-variavel-mensagem/src/App.js
+++ 3-projects/4-botao-alerta-variavel-mensagem/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const mensagemAlerta = 'mensagem de alerta' 
+
+  const mostrarAlerta = () => {
+    alert(mensagemAlerta);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Projeto de Botão Alerta</h1>
+        <button onClick={mostrarAlerta}>Clique Aqui</button>
       </header>
     </div>
   );
