b6e28989677f577a7c4109f08172217c7c11479d
philipheferreira <philipheferreira@gmail.com>
2025-09-03 12:00:44 -0300

    Feat: create last modal in project

--- 1-Components/20-primeiro-modal/src/App.jsx
+++ 1-Components/20-primeiro-modal/src/App.jsx
@@ -16,8 +16,8 @@ function App() {
         {estaAberto && (
         <div className="modal-overlay" onClick={() => setEstaAberto(false)}>
           <div className="modal" onClick={(e) => e.stopPropagation()}>
-            <h2>Título do Modal</h2>
-            <p>Esse é um exemplo simples de modal em React com CSS puro.</p>
+            <h2 style={{color:'#333'}}>Título do Modal</h2>
+            <p style={{ color: '#333', fontSize: '14px' }}>Esse é um exemplo simples de modal em React com CSS puro.</p>
             <div className="modal-actions">
               <button className="cancel-btn" onClick={() => setEstaAberto(false)}>
                 Cancelar
