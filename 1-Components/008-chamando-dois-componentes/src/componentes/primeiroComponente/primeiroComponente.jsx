22fa6ec143cf9b5dc1e5d67413d219182753afb4
philipheferreira <philipheferreira@gmail.com>
2025-09-04 15:57:06 -0300

    Feat: corrections in project 8

---
+++ 1-Components/008-chamando-dois-componentes/src/componentes/primeiroComponente/primeiroComponente.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './stylePrimeiroComponente.css';
+
+function PrimeiroComponente() {
+  return (
+    <div>
+      <h2 style={{ color: 'white' }}>Olá, eu sou o Primeiro componente componente!</h2>
+      <p>Esse é um texto vindo de um componente React.</p>
+    </div>
+  );
+}
+
+export default PrimeiroComponente;
---
+++ 1-Components/008-chamando-dois-componentes/src/componentes/segundoComponente/segundoComponente.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './syleSegundoComponente.css';
+
+function SegundoComponente() {
+  return (
+    <div>
+      <h2 style={{ color: 'white' }}>Olá, eu sou o Segundo componente!</h2>
+      <p>Esse é um texto vindo de um componente React.</p>
+      <SegundoComponente />
+    </div>
+  );
+}
+
+export default SegundoComponente;
