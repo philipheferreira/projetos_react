2bd6503e209c7abb7c87ad5cf148ed01368e84bf
philipheferreira <philipheferreira@gmail.com>
2025-09-04 16:32:56 -0300

    Feat: adjust project in 008

---
+++ 1-Components/008-chamando-dois-componentes/src/componentes/primeiroComponente/primeiroComponente.jsx
@@ -0,0 +1,12 @@
+import './stylePrimeiroComponente.css';
+
+function PrimeiroComponente() {
+  return (
+    <div>
+      <h2 style={{ color: 'white' }}>Olá, eu sou o Primeiro componente componente!</h2>
+      <p>Esse é um texto vindo de um componente React.</p>
+    </div>
+  );
+}
+
+export default PrimeiroComponente;
---
+++ 1-Components/008-chamando-dois-componentes/src/componentes/segundoComponente/segundoComponente.jsx
@@ -0,0 +1,12 @@
+import './styleSegundoComponente.css';
+
+function SegundoComponente() {
+  return (
+    <div>
+      <h2 style={{ color: 'white' }}>Olá, eu sou o Segundo componente!</h2>
+      <p>Esse é um texto vindo de um componente React.</p>
+    </div>
+  );
+}
+
+export default SegundoComponente;
