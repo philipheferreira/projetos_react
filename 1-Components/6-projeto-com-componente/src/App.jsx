968a6d633996414ce66d57b2a30997285b2630f9
philipheferreira <philipheferreira@gmail.com>
2025-09-04 11:22:33 -0300

    Feat: create component

---
+++ 1-Components/6-projeto-com-componente/src/App.jsx
@@ -0,0 +1,16 @@
+import MeuComponente from './components/MeuComponente/MeuComponente.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <MeuComponente />
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ 1-Components/6-projeto-com-componente/src/components/MeuComponente/MeuComponente.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MeuComponente() {
+  return (
+    <div>
+      <h2>Olá, eu sou o componente!</h2>
+      <p>Esse é um texto vindo de um componente React.</p>
+    </div>
+  );
+}
+
+export default MeuComponente;
