f620311cd52af6d55524a4c21756d128a6c7dd56
philipheferreira <philipheferreira@gmail.com>
2025-09-05 15:01:29 -0300

    Feat: create modal in project 21

--- 1-Components/021-modal-chama-modal/src/App.jsx
+++ 1-Components/021-modal-chama-modal/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import PrimeiroModal from './Componentes/Modais/PrimeiroModal/PrimeiroModal'
 import './App.css'
 
 function App() {
@@ -8,12 +9,7 @@ function App() {
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <PrimeiroModal />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
