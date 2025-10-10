27931ca9ea7a2e9fefde1e993338696035076c35
philipheferreira <philipheferreira@gmail.com>
2025-10-10 15:52:24 -0300

    Feat: adjust somehings

--- 4-projects/6-to-do-list-project1/src/Components/to-do-list.jsx
+++ 4-projects/6-to-do-list-project1/src/Components/to-do-list.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 
 function ToDoList() {
-    
-        const [task, newTasks] = useState([]);
+
+        const [tasks, setTasks] = useState(["Eat Breakfast", "Go to Gym", "Read a Book"]);
         const [newTask, setNewTask] = useState('');
 
         function handleInputChange(event) {
-
+            setNewTask(event.target.value);
         }
 
         function addTask() {
@@ -27,9 +27,48 @@ function ToDoList() {
 
     return(
     <div>
-        <p className="read-the-docs">
-            Eu sou um componente que está funcionando
-        </p>
+        <h1>To-Do List</h1>
+
+        <div>
+            <input
+                type="text"
+                placeholder='Enter a task...'
+                value={newTask}
+                onChange={handleInputChange}
+            />
+            <button
+                className='addTask'
+                onClick={addTask}
+            >
+                Add Task
+            </button>
+        </div>
+
+        <ol>
+            {tasks.map((task, index) => 
+                <li key={index}>
+                    <span className='text'>{task}</span>
+                    <button
+                        className='delete-button'
+                        onClick={deleteTask(index)}
+                    >
+                        Delete
+                    </button>
+                    <button
+                        className='move-button'
+                        onClick={() => deleteTask(index)}
+                    >
+                        Up
+                    </button>
+                    <button
+                        className='move-button'
+                        onClick={() => moveTaskDown(index)}
+                    >
+                        Down
+                    </button>
+                </li>
+            )}        
+        </ol>
     </div>
     );
 }
