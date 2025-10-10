a8a7388d49246ee71553984ec859a3c1fb8756fd
philipheferreira <philipheferreira@gmail.com>
2025-10-10 14:17:33 -0300

    Feat: crate new project

---
+++ 4-projects/to-do-list/src/Components/to-do-list.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function ToDoList() {
+    
+        const [task, newTasks] = useState([]);
+        const [newTask, setNewTask] = useState('');
+
+        function handleInputChange(event) {
+
+        }
+
+        function addTask() {
+
+        }
+
+        function deleteTask(index) {
+        
+        }
+
+        function moveTaskUp(index) {
+
+        }
+
+        function moveTaskDown(index) {
+
+        }
+
+    return(
+    <div>
+        Eu sou um componente
+    </div>
+    );
+}
+export default ToDoList;
